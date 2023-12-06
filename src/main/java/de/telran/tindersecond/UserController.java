2aa4f9d1e7b2995ce54890d586acd8038d781cfe
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-12-06 20:59:43 +0300

    commit

--- src/main/java/de/telran/tindersecond/UserController.java
+++ src/main/java/de/telran/tindersecond/UserController.java
@@ -1,19 +1,15 @@
 package de.telran.tindersecond;
 
 import de.telran.tindersecond.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //создается контроллер и управляется спрингом
+@RequiredArgsConstructor
 public class UserController {
-    
-    private final UserService userService;
 
-    public UserController(@Autowired @Qualifier(value = "randomUserService") UserService userService) { //Находит userService в контексте приложения и внедряет зависимость (заполняет переменную)
-        this.userService = userService;
-    }
+    private final UserService userService;
 
     @GetMapping(value = "/show-new-user")
     public User showNewUser() {
